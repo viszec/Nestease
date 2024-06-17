cb427903fdf718b963d0333689d79559422c6674
Mavis Ma <s3621134@student.rmit.edu.au>
2024-06-18 00:59:02 +1000

    add redux and update login/register

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "../styles/Login.scss";
+import { setLogin } from "../redux/state";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,24 +26,36 @@ const LoginPage = () => {
 
       /* Get data after fetching */
       const loggedIn = await response.json();
-    } catch (err) {}
+
+      if (loggedIn) {
+        dispatch(
+          setLogin({
+            user: loggedIn.user,
+            token: loggedIn.token,
+          })
+        );
+        navigate("/");
+      }
+    } catch (err) {
+      console.log("Login failed", err.message);
+    }
   };
   return (
     <div className="login">
       <div className="login_content">
-        <form className="login_content_form">
+        <form className="login_content_form" onSubmit={handleSubmit}>
           <input
             type="email"
             placeholder="Email"
             value={email}
-            onChange={(e) => e.target.value}
+            onChange={(e) => setEmail(e.target.value)}
             required
           />
           <input
             type="password"
             placeholder="Password"
             value={password}
-            onChange={(e) => e.target.value}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
           <button type="submit">Log in</button>
