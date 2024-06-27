e9e49911917baac98be0b4a03f6854c4d9c2ce8a
Mavis Ma <s3621134@student.rmit.edu.au>
2024-06-27 14:39:03 +1000

    rebuild server api on vercel

--- server/api/index.js
+++ server/api/index.js
@@ -4,10 +4,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
-const authRoutes = require("./routes/auth.js")
-const userRoutes = require("./routes/user.js")
-const listingRoutes = require("./routes/listing")
-const bookingRoutes = require("./routes/booking.js")
+const authRoutes = require("../routes/auth.js")
+const userRoutes = require("../routes/user.js")
+const listingRoutes = require("../routes/listing.js")
+const bookingRoutes = require("../routes/booking.js")
+
+// 添加这行来打印 MONGO_URL
+console.log("MONGO_URL:", process.env.MONGO_URL);
+console.log("PORT:", process.env.PORT);
 
 app.use(cors());
 app.use(express.json());
@@ -20,8 +24,6 @@ app.use("/properties", listingRoutes)
 app.use("/bookings", bookingRoutes)
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
 
 /* MONGOOSE SETUP */
 //const PORT = 3001;
@@ -39,4 +41,4 @@ mongoose
     })
     .catch((err) => console.log(`${err} did not connect`));
 
-
+module.exports = app;
